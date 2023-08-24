f5a3edca4642c0d22d1147c0269c573a9ec06f4f
Juan Darío Penagos Mejía <Jpenagos@intergrupo.com>
2023-08-23 22:16:33 -0500

    ajustes

--- src/cinemas/CreateCinemas.tsx
+++ src/cinemas/CreateCinemas.tsx
@@ -1,5 +1,9 @@
 export default function CreateCinemas(){
     return(
-        <h3>Crear Cines</h3>
+        <><h3>Crear Cines</h3>
+        <button className="btn btn-success mr-1">Success</button>
+        
+        <button className="btn btn-danger mr-1" >Danger</button>
+        </>
     )
 }
